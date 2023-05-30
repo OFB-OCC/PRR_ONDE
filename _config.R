ee37d82aef6fd567240865c66551b39125a55d50
OFB-OCC <123543390+OFB-OCC@users.noreply.github.com>
2023-05-30 16:00:12 +0200

    Adaptation au traitement de la région Occitanie

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
 ## configuration
-conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Normandie"
-conf_auteur <- "OFB - Direction régionale Normandie"
-conf_dep <- c("14", "27", "50", "61", "76")
+conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Occitanie"
+conf_auteur <- "OFB - Direction régionale Occitanie"
+conf_dep <- c("09", "11", "12", "30", "31", "32", "34", "46", "48", "65", "66", "81", "82")
